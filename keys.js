b483ca4cd2fc74ce34695f55aef5933fc29d3c8f
Danny <DJ@MacBook-Pro.local>
2017-08-01 21:42:18 -0600

    rock it

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+exports.twitterKeys = {
+  consumer_key:'47pRT5frywwVhUPXSORKdCeLB',
+  consumer_secret:'ZJAW7wxj2cn3E0fFxa5WVLekA4nVlapbstkgT10AnkRV8KohnT',
+  access_token_key:'277791278-0NwCXrTyjeRxe66KYSDL21LguGCSVOhGR7mlEW9J',
+  access_token_secret:'Ik4xjFlywFf4ILM6TKYonzm7nDUimAPoxX4nmjDiYKYTy',
+};
+
+
+ 
+exports.spotify = {
+  id: 'a0deba99959644a198599c92fa9aa33c',
+  secret: '46b368953d2c46df98908dce126bac72',
+};
